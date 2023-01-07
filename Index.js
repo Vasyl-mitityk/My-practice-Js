6a77560d98a53ba48b68785a22f11eecdcd0dc8a
Vasyl-mitityk <Vipsvob@gmail.com>
2023-01-07 14:15:07 +0200

    function

---
+++ Index.js
@@ -0,0 +1,19 @@
+// function multByFactor(value, multiplier = 1) {
+//   return value * multiplier;
+// }
+
+// const multByFactor = function (value, multiplier = 1) {
+//   return value * multiplier;
+// };
+
+// const multByFactor = (value, multiplier = 1) => value * multiplier;
+
+//
+
+const multByFactor = (value, multiplier = 1) => {
+  return value * multiplier;
+};
+
+console.log(multByFactor(20, 4));
+console.log(multByFactor(4, 0));
+console.log(multByFactor(40));
